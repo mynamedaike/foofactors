ad5dfa277da74aa690a2c95e108c9e5119a26a66
Ke Dai <foreversuperdk@163.com>
2016-12-01 15:45:11 -0800

    Document fbind()

--- R/fbind.R
+++ R/fbind.R
@@ -10,4 +10,8 @@
 #' @export
 #'
 #' @examples
-#'#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
+}
